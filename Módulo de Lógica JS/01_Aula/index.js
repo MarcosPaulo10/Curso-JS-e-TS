d59fd6d227035c9e94cef97ec089a93bc6abd282
Marcos Paulo Nogueira Mendes <mpnm10@hotmail.com>
2024-09-17 17:39:43 -0300

    ----

---
+++ Módulo de Lógica JS/01_Aula/index.js
@@ -0,0 +1,33 @@
+/*
+Operadores de comparação:
+
+> maior que
+>= maior que ou igual a
+< menor que
+<= menor que ou igual a
+=== igualdade estrita (valor e tipo)
+!== diferente estrito (valor e tipo)
+
+*/
+
+const num1 = 8;
+const num2 = 2;
+
+let comp = num1 !== num2;
+console.log(comp);
+
+comp = num1 > num2;
+console.log(comp);
+
+comp = num1 >= num2;
+console.log(comp);
+
+comp = num1 <= num2;
+console.log(comp);
+
+comp = num1 < num2;
+console.log(comp);
+
+comp = num1 == num2;
+console.log(comp);
+
