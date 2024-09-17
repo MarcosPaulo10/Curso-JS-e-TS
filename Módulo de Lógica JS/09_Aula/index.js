fada54ccb271d57874e47dec209d801761058571
Marcos Paulo Nogueira Mendes <mpnm10@hotmail.com>
2024-09-17 17:40:29 -0300

    ---

---
+++ Módulo de Lógica JS/09_Aula/index.js
@@ -0,0 +1,9 @@
+for (let i = 0; i <= 5; i++) {
+    console.log(i)
+}
+
+const frutas = ['Maçã', 'Pêra', 'Uva'];
+
+const novasFrutas = frutas.map(el => el.toLowerCase());
+
+console.log(novasFrutas); // [ 'maçã', 'pêra', 'uva' ]
